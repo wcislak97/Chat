2643998ae32d4c1066b4250628ebb3d0fb015f2d
anapieralska <agata.napieralska1205@gmail.com>
2022-01-23 16:06:24 +0100

    added basic ui stuff

--- src/main/java/com/example/chat/ClientController.java
+++ src/main/java/com/example/chat/ClientController.java
@@ -1,14 +1,25 @@
 package com.example.chat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class ClientController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView listViewChatMessages;
+    @FXML
+    private Button btnSend;
+    @FXML
+    private TextArea txtAreaMessage;
+
+    @FXML
+    protected void onSendButtonClicked(ActionEvent event) {
+        String message = txtAreaMessage.getText();
+
+        listViewChatMessages.getItems().addAll(message);
+
     }
+
+
 }
