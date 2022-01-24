73e0b9ebc02f060637f4f27d149d81596be0bb0f
anapieralska <agata.napieralska1205@gmail.com>
2022-01-24 22:23:38 +0100

    added db connection

--- src/main/java/com/example/chat/Client.java
+++ src/main/java/com/example/chat/Client.java
@@ -1,13 +1,18 @@
 package com.example.chat;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.*;
+import java.net.*;
+import com.example.chat.DatabaseConnection;
 
 import java.io.IOException;
 
 public class Client extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Server.class.getResource("chat_scene.fxml"));
@@ -19,5 +24,14 @@ public class Client extends Application {
 
     public static void main(String[] args) {
         launch();
+
+        /*
+        DatabaseConnection mySQL=new DatabaseConnection();
+
+            mySQL.selectStatement("SELECT * FROM table_name WHERE condition");
+            mySQL.insertStatement("INSERT INTO table_name (col1,col2) VALUES (val1, val2)");
+            mySQL.updateStatement("UPDATE table_name SET col1=val1, col2=val2 WHERE condition");
+        */
+
     }
 }
