b85e6a61e1e9016f544ca555248717589a2305cd
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-01-30 23:25:04 +0100

    Add ClientConnectionService

---
+++ src/main/java/com/example/chat/ClientConnectionService.java
@@ -0,0 +1,54 @@
+package com.example.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientConnectionService extends Thread {
+
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public ClientConnectionService() {
+        try {
+            this.socket = new Socket("localhost", 9999);
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String sendMessage(String msg) {
+        try {
+            writer.println(msg);
+            String resp = null;
+            resp = reader.readLine();
+            return resp;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void stopConnection() {
+        try {
+            reader.close();
+            writer.close();
+            this.socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public  void run() {
+        // listen to events from server
+        this.sendMessage("suchenia wydupcaj");
+    }
+
+}
